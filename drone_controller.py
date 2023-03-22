62ab80e828f124d9600ac1b469ce55ba88d6be29
Siyai Dhure <111302612+SiyaiDhure@users.noreply.github.com>
2023-03-22 22:51:31 +0200

    Update drone_controller.py

--- drone_controller.py
+++ drone_controller.py
@@ -1,5 +1,23 @@
 import bge
 from bge import *
 
-class drone(types.KX_PythonComponent):
-  
+class drone(bge.types.KX_PythonComponent):
+
+    args = {
+        'FL_Prop': '',
+        'FR_Prop': '',
+        'BL_Prop': '',
+        'FR_Prop': '',
+        'Body': ''
+    }
+
+    def start(self, args):
+
+        scene = bge.logic.getCurrentScene()
+        prefarbs = scene.objects
+
+        self.FL_Prop = prefarbs[args['FL_Prop']]
+
+
+    def update(self):
+        self.FL_Prop.applyMovement([0.02,0,0], True)
